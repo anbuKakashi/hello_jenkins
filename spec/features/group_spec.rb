bde31ffc7da002278da418611c14f84c1eec8528
anbuKakashi <kakafuad12@gmail.com>
2018-07-09 12:58:10 +0700

    Create group_spec.rb

---
+++ spec/features/group_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+ 
+describe "Sign in process" do
+
+    it "Sign me in" do
+#      visit "/" do
+#      visit "/admins/sign_in" do
+#        within("#new_admin") do
+#          fill_in '#admin_email', with: 'admin@sakanti.com'
+#          fill_in '#admin_password', with: '12caption3'
+#        end
+#        click_button 'Sign in'
+#        expect('hayy bos !!').to have_content 'hayy bos !!'
+      end
+    end
+
+#end
